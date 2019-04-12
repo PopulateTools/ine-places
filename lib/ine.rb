3b09d04bf17e245c8a5712d470675a34552d047e
Fernando Blat <ferblape@gmail.com>
2019-04-12 16:44:24 +0200

    Use zeitwerk to load constants

--- lib/ine.rb
+++ lib/ine.rb
@@ -3,19 +3,28 @@ require "csv"
 require "ostruct"
 require "active_support/all"
 require "open-uri"
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+
+class INEInflector < Zeitwerk::Inflector
+  def camelize(basename, _abspath)
+    case basename
+    when "csv_record"
+      "CSVRecord"
+    when "ine"
+      "INE"
+    else
+      super
+    end
+  end
+end
+
+loader.inflector = INEInflector.new
+loader.setup
 
 module INE
   module Places
-
-    require "ine/places/csv_record"
-    require "ine/places/autonomous_region"
-    require "ine/places/autonomous_regions_collection"
-    require "ine/places/province"
-    require "ine/places/provinces_collection"
-    require "ine/places/place"
-    require "ine/places/places_collection"
-
-    ROOT = File.expand_path('../places/', __FILE__)
+    ROOT = File.expand_path('../ine/places/', __FILE__)
 
     def self.preload
       AutonomousRegionsCollection.records
--- lib/ine/places/version.rb
+++ lib/ine/places/version.rb
@@ -1,5 +1,5 @@
 module INE
   module Places
-    VERSION = "0.3.0"
+    VERSION = "0.4.0"
   end
 end
